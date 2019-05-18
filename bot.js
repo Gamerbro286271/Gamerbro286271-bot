28af6aad660fa03fdcd8b804dd3d4d0364b19686
Gamerbro286271 <49330564+Gamerbro286271@users.noreply.github.com>
2019-05-17 22:27:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,30 @@ client.on('guildMemberAdd', member => {
 	channel.send(`Welcome to the server, ${member}`);
 });
 
+bot.on('message', message => {
+	if (!message.guild) return;
+
+	if (message.content.startsWith('!kick')) {
+	  const user = message.mentions.users.first();
+	  if (user) {
+		if(!message.member.roles.find(r => r.name === "STAFF")) return message.channel.send('YOU DO NOT HAVE PERMISSIONS')
+		const member = message.guild.member(user);
+		if (member) {
+		  member.kick('Optional reason that will display in the audit logs').then(() => {
+			message.reply(`Successfully kicked ${user.tag}`);
+		  }).catch(err => {
+			message.reply('I was unable to kick the member');
+			console.error(err);
+		  });
+		} else {
+		  message.reply('That user isn\'t in this guild!');
+		}
+	  } else {
+		message.reply('You didn\'t mention the user to kick!');
+	  }
+	}
+  });
+
   client.on('message', message => {
 	if (!message.guild) return;
   
