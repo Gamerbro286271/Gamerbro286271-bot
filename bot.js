c068949b7daab3146f455275ef483827152c9901
Gamerbro286271 <49330564+Gamerbro286271@users.noreply.github.com>
2019-05-17 22:21:27 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,31 @@ client.on('guildMemberAdd', member => {
 	channel.send(`Welcome to the server, ${member}`);
 });
 
+  bot.on('message', message => {
+	if (!message.guild) return;
+  
+	if (message.content.startsWith('!ban')) {
+	  const user = message.mentions.users.first();
+	  if (user) {
+		if(!message.member.roles.find(r => r.name === "Administrator")) return message.channel.send('YOU DO NOT HAVE PERMISSIONS')
+		const member = message.guild.member(user);
+		if (member) {
+		  member.ban({
+			reason: 'They were bad!',
+		  }).then(() => {
+			message.reply(`Successfully banned ${user.tag}`);
+		  }).catch(err => {
+			message.reply('I was unable to ban the member');
+			console.error(err);
+		  });
+		} else {
+		  message.reply('That user isn\'t in this guild!');
+		}
+	  } else {
+		message.reply('You didn\'t mention the user to ban!');
+	  }
+	}
+  });
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
